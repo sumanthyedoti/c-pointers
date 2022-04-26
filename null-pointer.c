e7ed70c6b05f4886bee2d7273fb32c7e7aac9360
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-04-26 22:38:27 +0530

    Add global and static pointers to NULL poniters

--- null-pointer.c
+++ null-pointer.c
@@ -5,7 +5,11 @@
  * NULL pointer does not point to any area of memory
  * NULL is defined in many libraries as:
  *      #define NULL ((void *)0)
+ *
+ * If a pointer is declared as global or static, it is initialized to NULL
  */
+
+int *global_ptr; // NULL
 
 int main() {
   /** Two null pointers will be equal to each other */
@@ -20,9 +24,14 @@ int main() {
 
   /** A pointer can be used as the sole operand of a logical expression */
   if (p) {
-    printf("'p' is not a NULL pointer");
+    printf("'p' is not a NULL pointer\n");
   } else {
-    printf("'p' is a NULL pointer");
+    printf("'p' is a NULL pointer\n");
+  }
+
+  static int *static_ptr; // NULL
+  if (global_ptr == static_ptr) {
+    printf("Global and Static pointers are initialized to NULL\n");
   }
 
   return 0;
